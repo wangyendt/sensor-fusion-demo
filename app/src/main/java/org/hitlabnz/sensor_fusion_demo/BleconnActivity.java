cdca5863e58e388432140d8e83c920abfd86f231
80301815 <905317742@qq.com>
2021-10-13 16:40:49 +0800

    实现界面调用

---
+++ app/src/main/java/org/hitlabnz/sensor_fusion_demo/BleconnActivity.java
@@ -0,0 +1,34 @@
+package org.hitlabnz.sensor_fusion_demo;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class BleconnActivity extends FragmentActivity {
+    Button btnClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bleconn);
+        Log.d("shit", "fuck");
+        btnClick = findViewById(R.id.btnJump);
+        Log.d("shit", "fuck2");
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("shit", "fuck3");
+                Intent intent = new Intent(
+                        BleconnActivity.this,
+                        SensorSelectionActivity.class
+                );
+//                startActivityForResult(intent, 0);
+                startActivity(intent);
+            }
+        });
+    }
+}
